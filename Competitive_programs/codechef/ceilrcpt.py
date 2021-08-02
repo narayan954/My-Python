0021a27d41aafa3b2edf8af88483330634d6d920
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-08-02 13:17:03 +0530

    Create ceilrcpt.py

---
+++ Competitive_programs/codechef/ceilrcpt.py
@@ -0,0 +1,12 @@
+menu=[2048,1024,512,256,128,64,32,16,8,4,2,1]
+for _ in range(int(input())):
+    p = int(input())
+    n = 0
+    ans=0
+    while n < 12:
+        ans += p//menu[n]
+        p = p%menu[n]
+        if p==0:
+            break
+        n += 1
+    print(ans)
