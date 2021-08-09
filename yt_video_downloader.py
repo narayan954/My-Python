eb8a0fc0737069ac7e29b343b71e9a8a14937dcf
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-08-10 02:49:03 +0530

    Update yt_video_downloader.py

--- yt_video_downloader.py
+++ yt_video_downloader.py
@@ -1,9 +1,11 @@
 from pytube import YouTube as yt
 
 link = input('Enter the video link : ')
+path = 'D:/'
 
-print('Downloading...................')
+print('Downloading..................')
 
-yt(link).streams.first().download()
+Myvideo = yt(link).streams.filter(file_extension = "mp4")
+Myvideo.first().download(path)
 
 print('Video downloaded successfully')
