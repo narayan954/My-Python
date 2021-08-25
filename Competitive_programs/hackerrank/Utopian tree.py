826992c5a8c5218bc42ae77090fe42578938f69a
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-08-25 22:07:47 +0530

    Create Utopian tree.py

---
+++ Competitive_programs/hackerrank/Utopian tree.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'utopianTree' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def utopianTree(n):
+    full_cycles = pow(2, n//2 + 1) - 1
+    if n%2 == 0:
+        return full_cycles
+    return full_cycles * 2      
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = utopianTree(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
