f4b5b782aa3d27bcbebb6f46f419eb0465fa2501
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-08-30 19:56:39 +0530

    Create Consistency chapter 1.py

---
+++ Competitive_programs/hackercup/Consistency chapter 1.py
@@ -0,0 +1,20 @@
+t = int(input())
+for j in range(1, t + 1):
+    s = input()
+    if len(s) == 1:
+        print(f"Case #{j}:", 0)
+        continue
+    a = 26 * [0]
+    x, y, h = 0, 0, 0
+    for i in s:
+        a[ord(i) - 65] += 1
+        if i == 'A' or i == 'E' or i == 'O' or i == 'U' or i == 'I':
+            x += 1
+        else:
+            y += 1
+            if a[ord(i) - 65] > h:
+                h = a[ord(i) - 65]
+    if x <= y:
+        print(f"Case #{j}:", y + 2 * (x - max(a[0], a[4], a[8], a[14], a[20])))
+    else:
+        print(f"Case #{j}:", x + 2 * (y - h))
