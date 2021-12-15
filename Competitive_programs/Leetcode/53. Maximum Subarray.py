6e548130fbc1462da39cf7a19a3993fd70102daa
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-12-15 11:52:17 +0530

    Update 53. Maximum Subarray.py
    Must Revise!!!

--- Competitive_programs/Leetcode/53. Maximum Subarray.py
+++ Competitive_programs/Leetcode/53. Maximum Subarray.py
@@ -5,7 +5,9 @@ class Solution:
             if i >= len(nums): return 0 if must_pick else -inf
             return max(nums[i] + solve(i+1, True), 0 if must_pick else solve(i+1, False))
         return solve(0, False)
+    
 """
+Using Divide and Conquer
 class Solution:
     def maxSubArray(self, nums):
         def maxSubArray(A, L, R):
